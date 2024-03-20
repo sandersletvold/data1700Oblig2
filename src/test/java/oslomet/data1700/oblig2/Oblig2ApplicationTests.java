351141ef907440bcfabb5dea03f2f607db6477e1
sandersletvold <143513259+sandersletvold@users.noreply.github.com>
2024-03-20 22:09:02 +0100

    Opprettet Spring prosjekt.

---
+++ src/test/java/oslomet/data1700/oblig2/Oblig2ApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.data1700.oblig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
